d81794bb8a4114b164f64f5f29fb92cfdb73dd5a
samirkhan-947 <samirrkhan947@gmail.com>
2024-11-03 01:37:14 +0530

    http request in HttpContext

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -3,4 +3,16 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.Run(async(HttpContext context) =>
+{
+    //context.Request
+   await context.Response.WriteAsync($"The method is:{context.Request.Method}\r\n");
+   await context.Response.WriteAsync($"The URL is:{context.Request.Path}\r\n");
+
+   await context.Response.WriteAsync($"\r\nHeaders:\r\n");
+    foreach (var key in context.Request.Headers.Keys)
+    {
+       await context.Response.WriteAsync($"{key}: {context.Request.Headers[key]}\r\n");
+    }
+}); 
 app.Run();
